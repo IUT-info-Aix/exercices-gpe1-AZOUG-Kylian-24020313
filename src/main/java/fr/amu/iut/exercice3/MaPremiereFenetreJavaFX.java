916381874f25284f27e10880d44fac9a55c72959
AZOUG-Kylian-24020313 <kylian.azoug@etu.univ-amu.fr>
2025-04-29 12:15:35 +0200

    tp2 exo 3 et 4

--- src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
+++ src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
@@ -1,9 +1,31 @@
 package fr.amu.iut.exercice3;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.event.EventHandler;
+import javafx.event.Event;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class MaPremiereFenetreJavaFX extends Application {
+
+    private Label helloLabel;
+    private TextField nameField;
+    private Button button;
+
+    // Gestionnaire d'évènements appelé lors du clic sur le bouton
+    EventHandler<MouseEvent> buttonClickHandler = actionEvent -> {
+        helloLabel.setText( "Bonjour à toi, "+nameField.getText() );
+    };
 
     public static void main(String[] args) {
         launch(args);
@@ -11,7 +33,58 @@ public class MaPremiereFenetreJavaFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        VBox vbox = new VBox();
+        vbox.setAlignment( Pos.CENTER );
+
+        // Création et ajout du label au conteneur
+        Label helloLabel = new Label("Bonjour à tous !");
+        vbox.getChildren().add( helloLabel );
+
+        // Création de la scene
+        Scene scene = new Scene( vbox );
+
+        // Ajout de la scene à la fenêtre
+        primaryStage.setScene( scene );
         primaryStage.setTitle("La page d'un Pro de JavaFX");
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(400);
         primaryStage.show();
+
+        TextField nameField = new TextField("Veuillez saisir un nom");
+        nameField.setMaxWidth(180.0d);
+        nameField.setFont( Font.font("Courier", FontWeight.NORMAL, 12) );
+        vbox.getChildren().add( nameField );
+
+        // Ajout d'un bouton avec du texte
+        Button button = new Button("Dire bonjour");
+        vbox.getChildren().add( button );
+
+        Image image = new Image( MaPremiereFenetreJavaFX.class.getResource("silver_button.png").toString() );
+
+        // Création d'un composant avec l'image peinte à l'intérieur
+        ImageView iv = new ImageView();
+        iv.setImage(image);
+
+        // Intégration de l'image dans le bouton
+        button.setGraphic( iv );
+
+    }
+    public class ButtonClickHandler implements EventHandler<Event> {
+
+        private Label helloLabel;
+        private TextField nameField;
+
+        public ButtonClickHandler(Label helloLabel, TextField nameField) {
+            this.helloLabel = helloLabel;
+            this.nameField = nameField;
+        }
+
+        @Override
+        public void handle(Event event) {
+            // Utiliser le Label récupéré
+            helloLabel.setText( "Bonjour à toi, "+nameField.getText() );
+        }
+
     }
 }
+
--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,31 +1,86 @@
 package fr.amu.iut.exercice4;
 
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+import javafx.scene.Scene;
+import javafx.animation.KeyFrame;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Palette extends Application {
 
-    private int nbVert = 0;
-    private int nbRouge = 0;
-    private int nbBleu = 0;
 
-    private Button vert;
-    private Button rouge;
-    private Button bleu;
+    private int compteurVert = 0;
+    private int compteurRouge = 0;
+    private int compteurBleu = 0;
 
-    private BorderPane root;
-    private Label label;
-    private Pane panneau;
-    private HBox bas;
+    private Pane zoneCouleur;
+    private Label affichage;
+    private Button boutonVert;
+    private Button boutonRouge;
+    private Button boutonBleu;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        zoneCouleur = new Pane();
+        zoneCouleur.setStyle("-fx-background-color: rgb(0,0,0);");
+        zoneCouleur.setPrefHeight(120);
+
+        // Label en haut
+        affichage = new Label("Cliquez sur un bouton pour changer la couleur");
+        HBox zoneTexte = new HBox(affichage);
+        zoneTexte.setAlignment(Pos.CENTER);
+        zoneTexte.setStyle("-fx-padding: 10px;");
+
+        // Boutons
+        boutonVert = new Button("Vert");
+        boutonRouge = new Button("Rouge");
+        boutonBleu = new Button("Bleu");
+
+        boutonVert.setOnAction(e -> miseAJourCouleur("vert"));
+        boutonRouge.setOnAction(e -> miseAJourCouleur("rouge"));
+        boutonBleu.setOnAction(e -> miseAJourCouleur("bleu"));
+
+        HBox zoneBoutons = new HBox(10, boutonRouge, boutonVert, boutonBleu);
+        zoneBoutons.setAlignment(Pos.CENTER);
+        zoneBoutons.setStyle("-fx-padding: 10px;");
+
+        // Placement global
+        BorderPane root = new BorderPane();
+        root.setTop(zoneTexte);
+        root.setCenter(zoneCouleur);
+        root.setBottom(zoneBoutons);
+
+        Scene scene = new Scene(root, 400, 200);
+        primaryStage.setTitle("Mélange de couleurs");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
+    }
+    private void miseAJourCouleur(String couleur) {
+        if (couleur.equals("vert")) compteurVert++;
+        else if (couleur.equals("rouge")) compteurRouge++;
+        else if (couleur.equals("bleu")) compteurBleu++;
+
+        int r = Math.min(compteurRouge * 40, 255);
+        int g = Math.min(compteurVert * 40, 255);
+        int b = Math.min(compteurBleu * 40, 255);
+
+        zoneCouleur.setStyle("-fx-background-color: rgb(" + r + "," + g + "," + b + ");");
+
+        affichage.setText(String.format("Rouge: %d | Vert: %d | Bleu: %d", compteurRouge, compteurVert, compteurBleu));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
 
