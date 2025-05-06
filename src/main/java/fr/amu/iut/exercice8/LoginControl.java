c6fe4c38f7c45ce18e4d1e24ab92e42e5c3ea76e
AZOUG-Kylian-24020313 <kylian.azoug@etu.univ-amu.fr>
2025-05-06 16:34:15 +0200

    exercice 8 tp 3 complet

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,31 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
-
+    @FXML
+    private TextField userId;
+    @FXML
+    private PasswordField pwd;
+    @FXML
     private void okClicked() {
-    }
+        String user = userId.getText();
+        int len = pwd.getText() != null ? pwd.getText().length() : 0;
 
+        // construits une suite de '*' de la bonne taille
+        StringBuilder stars = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            stars.append('*');
+        }
+
+        System.out.println(user + " " + stars);
+    }
+    @FXML
     private void cancelClicked() {
+        userId.clear();
+        pwd.clear();
     }
 }
